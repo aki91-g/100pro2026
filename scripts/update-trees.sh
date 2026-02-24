0f0116e5150d8194f0b8af643e9793e5163e7214
aki-91g <azuki.neu.tec@proton.me>
2026-02-24 03:55:20 +0000

    fix: ignore temp file

--- scripts/update-trees.sh
+++ scripts/update-trees.sh
@@ -12,7 +12,7 @@ for target in "${targets[@]}"; do
         echo "Updating $file..."
         echo '[TREE-START]' > temp_tree.txt
         echo '```text' >> temp_tree.txt
-        tree "$dir" -I 'node_modules|dist|target|.git|.github|.vscode|build|.DS_Store' -L 3 --dirsfirst --noreport >> temp_tree.txt
+        tree "$dir" -I 'node_modules|dist|target|.git|.github|.vscode|build|.DS_Store|temp_tree.txt' -L 3 --dirsfirst --noreport >> temp_tree.txt
         echo '```' >> temp_tree.txt
         echo '[TREE-END]' >> temp_tree.txt
         perl -0777 -i -pe 's/\[TREE-START\].*?\[TREE-END\]/`cat temp_tree.txt`/ges' "$file"
